110abb61fe5544b25d3ec687c6a5876ba546e7f8
kevinstivencortes <kevinstivencortesmaigual36@gmail.com>
2024-03-29 15:20:02 -0500

    cambios para ver errores2

--- back-end/node-js/src/controllers/mantenimientoController.js
+++ back-end/node-js/src/controllers/mantenimientoController.js
@@ -72,41 +72,26 @@ export const listarMantenimientoPorId_actividades = async (req, res) => {
         res.status(500).json({ "message": "Error en el controlador mantenimientoController.js: " + err });
     }
 };
-/* requerimiento 5 */
-export const listarRequerimiento5 = async (req, res) => {
+
+/* funcional */
+export const listarMantenimientoPorId = async (req, res) => {
     try {
-        let sql = `
-            SELECT mant_fecha_proxima, mant_fk_fichas, mant_fecha_realizacion, fichas.fi_placa_sena AS nombre_maquina, actividades.acti_nombre AS actividades_maquina, usuarios.us_nombre AS nombre_tecnico 
-            FROM mantenimiento 
-            LEFT JOIN fichas ON mantenimiento.mant_fk_fichas = fichas.idFichas 
-            LEFT JOIN actividades ON actividades.fk_mantenimiento = mantenimiento.idMantenimiento 
-            LEFT JOIN tecnicos_has_actividades ON actividades.idActividades = tecnicos_has_actividades.fk_actividades 
-            LEFT JOIN usuarios ON tecnicos_has_actividades.fk_usuarios = usuarios.idUsuarios
-            WHERE mant_fecha_proxima IS NOT NULL 
-
-
-            /* esto es para que solo las tablas que tengan todos los datos en todas las tablas aparescan */
-            /* AND mant_fk_fichas IS NOT NULL 
-            AND mant_fecha_realizacion IS NOT NULL 
-            AND fichas.fi_placa_sena IS NOT NULL 
-            AND actividades.acti_nombre IS NOT NULL 
-            AND usuarios.us_nombre IS NOT NULL; */
-        `;
+        const { idMantenimiento } = req.params;
+        let sql = `SELECT mant_codigo_mantenimiento, mant_fecha_realizacion, mant_fecha_proxima, mant_fk_fichas, fk_tipo_mantenimiento, mant_descripcion FROM mantenimiento WHERE idMantenimiento = ${idMantenimiento}`;
         const [result] = await conexion.query(sql);
 
-        /* verificar si se encontraron requerimientos de mantenimiento */
         if (result.length > 0) {
             res.status(200).json(result);
         } else {
-            res.status(404).json({ "message": "No se encontraron los requerimientos completos en la base de datos." });
+            res.status(404).json({ "message": "No se encontró mantenimiento con el ID especificado" });
         }
-    }
-    catch (err) {
-        res.status(500).json({ "message": "Error en el controlador listarRequerimiento5: " + err.message });
+    } catch (err) {
+        res.status(500).json({ "message": "Error en el controlador mantenimientoController.js: " + err });
     }
 };
 
-/* requerimiento 14 */
+
+/* funcional*/
 export const registrarMantenimiento = async (req, res) => {
     try {
         let {
@@ -190,23 +175,6 @@ export const listarRequerimiento17 = async (req, res) => {
 };
 
 /* funcional */
-export const listarMantenimientoPorId = async (req, res) => {
-    try {
-        const { idMantenimiento } = req.params;
-        let sql = `SELECT mant_codigo_mantenimiento, mant_fecha_realizacion, mant_fecha_proxima, mant_fk_fichas, fk_tipo_mantenimiento, mant_descripcion FROM mantenimiento WHERE idMantenimiento = ${idMantenimiento}`;
-        const [result] = await conexion.query(sql);
-
-        if (result.length > 0) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({ "message": "No se encontró mantenimiento con el ID especificado" });
-        }
-    } catch (err) {
-        res.status(500).json({ "message": "Error en el controlador mantenimientoController.js: " + err });
-    }
-};
-
-/* funcional */
 export const eliminarMantenimiento = async(req, res) =>{
     try{
     let idMantenimiento = req.params.id_mantenimiento;
@@ -257,3 +225,36 @@ export const actualizarMantenimiento = async(req, res) =>{
         return res.status(500).json({"menssage":"error" + e});
     }
 }
+/* requerimiento 5 */
+export const listarRequerimiento5 = async (req, res) => {
+    try {
+        let sql = `
+            SELECT mant_fecha_proxima, mant_fk_fichas, mant_fecha_realizacion, fichas.fi_placa_sena AS nombre_maquina, actividades.acti_nombre AS actividades_maquina, usuarios.us_nombre AS nombre_tecnico 
+            FROM mantenimiento 
+            LEFT JOIN fichas ON mantenimiento.mant_fk_fichas = fichas.idFichas 
+            LEFT JOIN actividades ON actividades.fk_mantenimiento = mantenimiento.idMantenimiento 
+            LEFT JOIN tecnicos_has_actividades ON actividades.idActividades = tecnicos_has_actividades.fk_actividades 
+            LEFT JOIN usuarios ON tecnicos_has_actividades.fk_usuarios = usuarios.idUsuarios
+            WHERE mant_fecha_proxima IS NOT NULL 
+
+
+            /* esto es para que solo las tablas que tengan todos los datos en todas las tablas aparescan */
+            /* AND mant_fk_fichas IS NOT NULL 
+            AND mant_fecha_realizacion IS NOT NULL 
+            AND fichas.fi_placa_sena IS NOT NULL 
+            AND actividades.acti_nombre IS NOT NULL 
+            AND usuarios.us_nombre IS NOT NULL; */
+        `;
+        const [result] = await conexion.query(sql);
+
+        /* verificar si se encontraron requerimientos de mantenimiento */
+        if (result.length > 0) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ "message": "No se encontraron los requerimientos completos en la base de datos." });
+        }
+    }
+    catch (err) {
+        res.status(500).json({ "message": "Error en el controlador listarRequerimiento5: " + err.message });
+    }
+};
