e65f52a1702d42edc40eb348ce29bfb8701e08c6
pac0sanch3 <noriegatrujolloj123@gmail.com>
2024-04-23 09:52:13 -0500

    añadiendo tecnicos al listar actividades

--- back-end/node-js/index.js
+++ back-end/node-js/index.js
@@ -1,13 +1,7 @@
 import Express from "express"
 import bodyParser from "body-parser"
-import RutaUsuario from "./src/routes/UserRoutes.js";
-import RutaRol from "./src/routes/RolUserRouter.js";
 import ActivitiesRoutes from "./src/routes/ActivitiesRoutes.js"
-import rutaSitio from "./src/routes/sitiosRouter.js"
-import rutaSede from "./src/routes/sedesRouter.js"
-import rutaCentro from "./src/routes/centrosRouter.js"
-import rutaArea from "./src/routes/areasRouter.js"
-import rutaTipoSitio from "./src/routes/tipo_sitioRouter.js"
+
 
 const serve = Express()
 const port = 3000
@@ -18,20 +12,14 @@ serve.use(cors())
 
 serve.use(bodyParser.json())
 serve.use(bodyParser.urlencoded({extended : false}))
-serve.set('view engine','ejs')
-serve.set('views','./views')
+
+
 serve.use(Express.static('./public'))
 serve.get('/', (req,res) =>{
   res.status(200).json({"mensaje" : "Bienvenidos a MachinApp"})
 })
-serve.use('/user',RutaUsuario)
-serve.use('/rol',RutaRol)
+
 serve.use('/actividades',ActivitiesRoutes)
-serve.use('/tipositio',rutaTipoSitio)
-serve.use('/sitio',rutaSitio)
-serve.use('/sede', rutaSede)
-serve.use('/centro', rutaCentro)
-serve.use('/area', rutaArea)
 
 serve.use('/documents',(req,res)=>{
   res.render('documentacion.ejs')
