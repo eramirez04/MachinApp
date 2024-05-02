c64f468c9246f03e8b2e52c54eefcc1d0200abba
jhon-frand <frandlebaza@gmail.com>
2024-05-02 16:45:59 -0500

    empezando el front-end de actividades

--- front-end/machinapp/src/pages/Inicio.jsx
+++ front-end/machinapp/src/pages/Inicio.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom'
 
 import Nav from '../components/Nav'
 
+import RegistroActividades from '../components/Actividades/RegistroActividades'
+
+
 const Inicio=()=>  {
 
   return (
     <>
+      <RegistroActividades/>
       <Nav/>
       <h1 className='center ' >Inicio</h1>
     </>
