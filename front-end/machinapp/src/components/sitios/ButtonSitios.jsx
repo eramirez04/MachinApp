f3a96f62f62f244fbb58c2cabcb99159c4a4bb1e
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2024-05-16 13:07:22 -0500

    sitios,sedes,areas y ambientes

---
+++ front-end/machinapp/src/components/sitios/ButtonSitios.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ButtonSitios = () => {
+  return (
+    <>
+      <button className="btn btn-info btn-primary mt-10 ml-36">Ingresar</button>
+    </>
+  )
+}
+
+export default ButtonSitios
