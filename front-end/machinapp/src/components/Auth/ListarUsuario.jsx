864db264a33d7faed1b70edab7a054f88c04488c
emersson <emerssonramirez510@gmail.com>
2024-05-16 13:22:58 -0500

    modulo de usuarios, registro, listar usuario, login, y actualizar

--- front-end/machinapp/src/components/Auth/ListarUsuario.jsx
+++ front-end/machinapp/src/components/Auth/ListarUsuario.jsx
@@ -1,38 +1,201 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
+import InputSubmit from "../InputSubmit";
 
-import Api from "../Api";
+import ActualizarUsuario from "./ActualizarUsuario";
 
+import {
+  UserIcon,
+  DocumentTextIcon,
+  EnvelopeIcon,
+  UserCircleIcon,
+  HomeIcon,
+  TruckIcon,
+} from "@heroicons/react/24/outline";
 
-const BuscarUsuario = () =>{
-    const {id} = useParams()
-    const[usuario, setUsuario] = useState({nombre:'', email: ''})
+import api from "../Api";
 
-    const navegacion = useNavigate()
+let usuarioLocal = localStorage.getItem("token");
 
+const BuscarUsuario = () => {
+  // const {id} = useParams()
+  let id = 25;
+  const [usuario, setUsuario] = useState({});
 
-    useEffect(()=>{
-        const buscar =async () => {
-         try {
-            const response = await Api.get(`/user/listar/${id}`)
-            setUsuario(response)
-            console.log(usuario)
-         } catch (error) {
-            console.error(error.response.data)
-            alert(error.response.data.message)
-         }   
-        }
+  const [userAct, setUserAct] = useState({
+    nombre: "",
+    apellidos: "",
+    correo: "",
+  });
 
-        buscar()
-    }, [id])
+  const navegacion = useNavigate();
 
+  useEffect(() => {
+    const buscar = async () => {
+      try {
+        const response = await api.get(`user/listar/${id}`);
 
-    return (
-        <>
-        hola desde el perfil
-        </>
-    )
-} 
+        let user = {};
+        user = response.data.user[0];
+        console.log(user.us_nombre);
+        setUsuario(user);
+      } catch (error) {
+        /*  navegacion('/') */
+      }
+    };
+    buscar();
+  }, [id]);
 
-export default BuscarUsuario
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const ActualizarUser = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.put(`user/actualizar/${id}`, userAct);
+
+      console.log(response);
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+  return (
+    <>
+      <ActualizarUsuario />
+
+      {/*  <div>
+        <input type="email" value={usuario.us_correo}
+        onChange={(e)=> setUsuario({...usuario, name: e.target.value})}
+        />
+      </div>
+ */}
+      <div className="min-h-full">
+        <section className="bg-white shadow">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl flex font-bold tracking-tight text-gray-900">
+              <UserIcon className="h-10 w-10" aria-hidden="true" />
+              Perfíl de usuario
+            </h1>
+          </div>
+        </section>
+        <main>
+          <div className="mx-auto max-w-7xl py-3 sm:px-3 lg:px-5">
+            <div className=" p-4 sm:p-6 lg:p-8 rounded-lg shadow-md">
+              <div className=" text-base sm:text-lg lg:text-xl">
+                <form
+                  action=""
+                  className="space-y-8  md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
+                >
+                  <div>
+                    <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                      <img src="" alt="" />
+                    </div>
+                    <div className="flex">
+                      <button onClick={ActualizarUser}> actualizar</button>
+                    </div>
+                  </div>
+                  <div className="w-full">
+                    <div>
+                      <div className="p-3 sm:p-1 lg:flex-auto">
+                        <div className="mt-10 flex items-center gap-x-1">
+                          <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                            Informacion personal
+                          </h4>
+                          <div className="h-px flex-auto bg-gray-100"></div>
+                        </div>
+                        <ul className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+                          <li className="flex gap-x-4">
+                            <UserIcon className="h-6 w-6" aria-hidden="true" />{" "}
+                            <input
+                              type="text"
+                              onChange={(e) =>
+                                setUserAct({
+                                  nombre: e.target.value,
+                                })
+                              }
+                              defaultValue={usuario.us_nombre}
+                            />
+                          </li>
+                          <li className="flex gap-x-3">
+                            <UserIcon className="h-6 w-6" aria-hidden="true" />
+                            <input
+                              type="text"
+                              defaultValue={usuario.us_apellidos}
+                              onChange={(e) =>
+                                setUserAct({
+                                  ...userAct,
+                                  apellidos: e.target.value,
+                                })
+                              }
+                            />
+                          </li>
+                          <li className="flex gap-x-3">
+                            <EnvelopeIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                            <input
+                              type="email"
+                              defaultValue={usuario.us_correo}
+                              onChange={(e) =>
+                                setUserAct({
+                                  ...userAct,
+                                  correo: e.target.value,
+                                })
+                              }
+                            />
+                          </li>
+                          <li className="flex gap-x-3">
+                            {usuario.us_numero_documento}
+                          </li>
+                          <li className="flex gap-x-3">
+                            <DocumentTextIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                            {usuario.us_tipo_documento}
+                          </li>
+                        </ul>
+                      </div>
+
+                      <div className="mt-10 flex items-center gap-x-4">
+                        <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                          Más info
+                        </h4>
+                        <div className="h-px flex-auto bg-gray-100"></div>
+                      </div>
+                      <ul className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+                        <li className="flex gap-x-4">
+                          <UserCircleIcon
+                            className="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                          {usuario.rol_nombre}
+                        </li>
+                        <li className="flex gap-x-4">
+                          <TruckIcon className="h-6 w-6" aria-hidden="true" />
+                          {usuario.us_especialidad}
+                        </li>
+                        <li className="flex gap-x-4">
+                          <HomeIcon className="h-6 w-6" aria-hidden="true" />
+                          {usuario.us_empresa}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default BuscarUsuario;
--- front-end/machinapp/src/pages/Inicio.jsx
+++ front-end/machinapp/src/pages/Inicio.jsx
@@ -1,65 +1,161 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
+const navigation = [];
 
 //componentes
-import Footer from '../components/Footer.jsx'
-import RegistroUsuarios from '../components/Auth/RegistroUsuarios.jsx'
+import Footer from "../components/Footer.jsx";
+import RegistroUsuarios from "../components/Auth/RegistroUsuarios.jsx";
 
 // login
-import Login from '../components/Auth/Login.jsx'
+import Login from "../components/Auth/Login.jsx";
 
-const Inicio=()=>  {
-
+const Inicio = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-      <>
-        <div className="selection:-order-none">
-            <header className="py-2 bg-[#52BD8F] sm:py-2 shadow-2xl">
-                <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-4">
-                    <div className="flex items-center justify-between">
-                        <div className="">
-                            <span className='text-white font-bold'>MachinApp</span>
-                        </div>
-                        <div className="relative hidden md:items-center md:justify-center md:inline-flex group ">
-                                    <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>Registro</button>
-                                    <dialog id="my_modal_2" className="modal">
-                                    <div className="modal-box">
-                                        <RegistroUsuarios/>
-                                    </div>
-                                    <form method="dialog" className="modal-backdrop">
-                                        <button>close</button>
-                                    </form>
-                                    </dialog>
-                        </div>
-
-                    </div>
-                </div>
-            </header>
-            <div className="md:flex md:justify-center md:gap-14 md:items-center">
-                <div className="md:w-1/2 relative sm:px-6 lg:px-8 max-w-7xl pt-24 shadow-2xl">
-                    <div className="mb-5 grid items-center  lg:grid-cols-2 ">
-                        <div>
-                            <h1 className="text-3xl sm:text-3xl lg:text-3xl xl:text-6xl font-bold">Mantenimiento,
-                                Seguridad y Alerta</h1>
-                            <p className="mt-4 text-lg font-normal text-gray-400 sm:mt-8">
-                            Aquí, cada archivo es una pieza clave para mantener nuestras máquinas en su máximo esplendor.
-                            Descubre la organización estructurada que preserva la calidad de cada servicio.
-                            ¡Explora, registra y eleva el estándar de tus máquinas y equipos con nosotros!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="md:w-4/12">
-                    <div className=" bg-white p-6 rounded-lg shadow-2xl ">
-                    <Login/>
-                    
-                    </div>
-                </div>
-
+    <>
+      <div className="selection:-order-none">
+        <header className="absolute inset-x-0 bg-[#52BD8F] top-0 z-50">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
+          >
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
             </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <div className="relative hidden md:items-center md:justify-center md:inline-flex group ">
+                <button
+                  className="btn"
+                  onClick={() =>
+                    document.getElementById("my_modal_2").showModal()
+                  }
+                >
+                  Login
+                </button>
+                <dialog id="my_modal_2" className="modal">
+                  <div className="modal-box">
+                    <Login />
+                  </div>
+                  <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                  </form>
+                </dialog>
+              </div>
+            </div>
+          </nav>
+          <Dialog
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className="fixed inset-0 z-50" />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Log in
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </header>
+        <div className="md:flex pt-28 md:justify-center md:gap-16 md:items-center">
+          <div className="md:w-1/2  sm:px-6 lg:px-8 max-w-7xl pt-24 shadow-2xl">
+            <div className="mb-5 grid items-center  lg:grid-cols-2 ">
+              <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-3">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+                <div className="text-center">
+                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Mantenimiento, Seguridad y Alerta
+                  </h1>
+                  <p className="mt-6 text-lg leading-8 text-gray-600">
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                    qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                    occaecat fugiat aliqua.
+                  </p>
+                  <div className="mt-10 flex items-center justify-center gap-x-6">
+                    <button
+                      type="button"
+                      className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    >
+                      Default
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-4/12 p-10">
+            <div className=" bg-white p-10 rounded-lg shadow-2xl ">
+              <RegistroUsuarios />
+            </div>
+          </div>
         </div>
-        <Footer/>
+      </div>
+     {/*  <Footer /> */}
     </>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;
