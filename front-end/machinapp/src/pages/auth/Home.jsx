c2ecea9a06fe95a5783eab7130fa7fe9ea0fce57
emersson <emerssonramirez510@gmail.com>
2024-07-09 21:27:52 -0500

    dependencia prop-type, para tipar los componentes, se modifico el Layout.jsx como ejemplo

--- front-end/machinapp/src/pages/auth/Home.jsx
+++ front-end/machinapp/src/pages/auth/Home.jsx
@@ -1,11 +1,12 @@
 import Layout from "../../components/template/Layout.jsx";
-
 import Artboard from "../../components/organisms/Paginacentrar.jsx";
 
 const Home = () => {
   return (
     <>
-      <Layout contenido={<Artboard />} titlePage={"Home"} />
+      <Layout titlePage={"Home"}>
+        <Artboard />
+      </Layout>
     </>
   );
 };
