e47efb4f96744f02ddb299a7b5033c6d43c20d96
emersson <emerssonramirez510@gmail.com>
2024-07-13 12:46:23 -0500

    organizacion de componentes front, para una mayor compresion de archivos

---
+++ front-end/machinapp/src/components/molecules/Avatar.jsx
@@ -0,0 +1,13 @@
+import { Avatar } from "@nextui-org/react";
+
+export const AvatarCom = () => {
+  return (
+    <>
+      <Avatar
+        isBordered
+        radius="full"
+        src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+      />
+    </>
+  );
+};
