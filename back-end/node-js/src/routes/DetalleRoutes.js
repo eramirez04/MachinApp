083b065912e4c1c11a26753f72c88672a38f20ac
Camilo3008 <camilo.alvarez3008@gmail.com>
2024-07-22 22:31:33 -0500

    actualizando back-end a la nueva bd

--- back-end/node-js/src/routes/DetalleRoutes.js
+++ back-end/node-js/src/routes/DetalleRoutes.js
@@ -9,7 +9,7 @@ import { verificar } from '../middlewares/LoginMidleware.js'
 
 const rutaDetalle = Router()
 
-rutaDetalle.post('/registrar',verificar, validar_detalle, registrarDetalle)
+rutaDetalle.post('/registrar', verificar, validar_detalle, registrarDetalle)
 rutaDetalle.get('/listar', verificar, listarDetalle)
 rutaDetalle.put('/actualizar/:idDetalle',verificar, validar_detalle,  actualizarDetalle)
 rutaDetalle.delete('/eliminar/:idDetalle',verificar, eliminarDetalle)
--- front-end/machinapp/src/components/molecules/CardMaquinas.jsx
+++ front-end/machinapp/src/components/molecules/CardMaquinas.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 
-import ImgMaquinas from '../atoms/ImgMaquinas.jsx'
 
 const CardMaquinas = ({placa, serial, tipoEquipo, estado, img}) => {
     return (
@@ -8,7 +7,7 @@ const CardMaquinas = ({placa, serial, tipoEquipo, estado, img}) => {
         <div className="card w-72 bg-gray-100 shadow-xl flex m-7 hover:drop-shadow-2xl static h-96 ">
 
           <div className="bg-white h-48 grid justify-items-center rounded-t-2xl">
-            <figure className="rounded-t-2xl h-48"> <ImgMaquinas img={img}/> </figure> 
+            <figure className="rounded-t-2xl h-48"> {/* <ImgMaquinas img={img}/> */}aaa </figure> 
           </div>
 
           <div className="p-5 grid items-center h-48">
