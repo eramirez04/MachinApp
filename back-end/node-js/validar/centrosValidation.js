710f8a029a1870e0cf2a63b822323e64a0fc5464
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2024-07-22 20:19:15 -0500

    añadir cambios en backend respecto a BD nueva

--- back-end/node-js/validar/centrosValidation.js
+++
@@ -1,9 +0,0 @@
-import { check } from "express-validator"
-
-export const verificarCentro = [
-    check('cen_nombre','Datos inválidos').not().isEmpty().isLength({max:200,min:5}),
-    check('cen_descripcion','Datos inválidos').not().isEmpty().isLength({max:255,min:10}),
-    check('cen_regional','Datos inválidos').not().isEmpty().isLength({max:100,min:3}),
-    check('cen_municipio','Datos inválidos').not().isEmpty().isLength({max:100,min:3}),
-    check('cen_subdirector','Datos inválidos').not().isEmpty().isLength({max:100,min:5})
-]
