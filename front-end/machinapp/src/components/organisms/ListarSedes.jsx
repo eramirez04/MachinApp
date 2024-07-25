403ed48b8de3311dc5f97b4c204256c8d276ba5b
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2024-07-24 23:56:50 -0500

    info detallada sedes y ambientes y adicion de funcionalidades nuevas

--- front-end/machinapp/src/components/organisms/ListarSedes.jsx
+++ front-end/machinapp/src/components/organisms/ListarSedes.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { axiosCliente } from '../../service/api/axios'
 import ButtonC from '../atoms/buttons/BottonC'
+import { FaEdit } from "react-icons/fa"
+import { AiFillInfoCircle } from "react-icons/ai"
 
 const BuscarSedes = () => {
   const [sedes, setSedes] = useState([])
@@ -27,18 +29,21 @@ const BuscarSedes = () => {
           Este centro está ubicado en el departamento del Huila, municipio de Pitalito. Este centro cuenta con dos sedes a día de hoy.
         </p>
       </header>
-      <div className='flex flex-col items-center space-y-8 py-10'>
+      <div className='flex flex-col items-center space-y-14 pt-10 pb-24'>
         {sedes.map((sede) => (
           <div className='bg-white shadow-lg rounded-lg overflow-hidden w-full md:w-3/4 lg:w-2/3 transform transition-all hover:scale-105 hover:shadow-2xl' key={sede.idSede}>
-            <img
-              src={`http://localhost:3000/imagenes/${sede.img_sede}`}
-              alt={sede.sede_nombre}
-              className='w-full h-64 object-cover'
-            />
+            <div>
+              <img
+                src={`http://localhost:3000/imagenes/${sede.img_sede}`}
+                alt={sede.sede_nombre}
+                className='w-full h-72 object-cover'
+              />
+              <Link to={`/Sedes/InfoSede/${sede.idSede}`}><button className='absolute text-4xl top-4 right-4 text-blue-600 hover:cursor-pointer hover:text-blue-700'><AiFillInfoCircle/></button></Link>
+            </div>
+            <button className='absolute text-4xl bottom-[120px] right-4 text-orange-400 hover:cursor-pointer hover:text-orange-500'><FaEdit/></button>
             <div className='p-6'>
               <h2 className='text-2xl font-bold text-gray-800'>{sede.sede_nombre}</h2>
               <p className='text-gray-600 mt-2'>{sede.sede_direccion}</p>
-              <p className='text-gray-700 mt-4'>{sede.sede_descripcion}</p>
               <div className='mt-4 flex justify-end'>
                 <Link to={`/Sedes/${sede.idSede}`}>
                   <ButtonC bgColor="bg-green-400 hover:bg-green-600 text-white" name="Ingresar"/>
