c3b194129d337a839b702dd8f6c97ab5a0714fea
emersson <emerssonramirez510@gmail.com>
2024-08-12 08:51:54 -0500

    nuevas rutas para router.jsx

--- front-end/machinapp/src/router/Router.jsx
+++ front-end/machinapp/src/router/Router.jsx
@@ -16,16 +16,16 @@ import Sitios from "../pages/Sitios";
 /* import PanelControl from "../pages/PaneldeControl"; */
 import Sedes from "../pages/Sedes";
 import Areas from "../pages/Areas";
-import Ambientes from "../pages/Ambientes";
 import PaneldeControlUsuarios from "../pages/PaneldeControl";
 import MaquinasAmbiente from "../pages/MaquinasAmbiente";
 import InfoMaquina from "../pages/InfoMaquina";
 import { ViewFormFichaTecnica } from "../pages/fichastecnicas/ViewFormFichaEquipos";
 import { ViewFormFicha_De_mantenimiento } from "../pages/fichastecnicas/ViewFormFicha_de_mantenimiento";
 import { FichaSolicitudPage } from "../pages/fichastecnicas/ViewFormFichaSolicitudPage";
-
 import InfoAmbiente from "../pages/InfoAmbiente";
 import InfoSede from "../pages/InfoSede";
+import AreasGeneral from "../pages/AreasGeneral";
+import AmbientesGeneral from "../pages/AmbientesGeneral";
 
 export const AppRouter = () => {
   return (
@@ -41,18 +41,14 @@ export const AppRouter = () => {
             <Route path="/FIchas" element={<Fichas />} />
             <Route path="/Historial" element={<Historial />} />
             <Route path="/Maquinas" element={<Maquinas />} />
-            <Route path="/Sitios" element={<Sitios />} />
-
+            <Route path="/Sedes" element={<Sitios />} />
             <Route path="/Sedes/:idSede" element={<Sedes />} />
-            <Route path="/Areas/:idArea" element={<Areas />} />
-            <Route path="/Ambientes" element={<Ambientes />} />
+            <Route path="/Areas/:idArea" element={<Areas />} />{" "}
             <Route
               path="/crearfichaequipos"
               element={<ViewFormFichaTecnica />}
             />
-
             <Route path="/Panelcontrol" element={<PaneldeControlUsuarios />} />
-
             <Route
               path="/MaquinasAmb/:idAmbiente"
               element={<MaquinasAmbiente />}
@@ -68,6 +64,8 @@ export const AppRouter = () => {
               element={<ViewFormFicha_De_mantenimiento />}
             />
             <Route path="/crearsolicitud" element={<FichaSolicitudPage />} />
+            <Route path="/Areas" element={<AreasGeneral />} />
+            <Route path="/Ambientes" element={<AmbientesGeneral />} />
           </Route>
         </Routes>
       </Suspense>
