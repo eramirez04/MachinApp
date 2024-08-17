04f5310482c88673ae4ab5a15d1738bcd44c50ab
emersson <emerssonramirez510@gmail.com>
2024-08-17 11:38:08 -0500

    mejorando componentes

--- front-end/machinapp/src/components/molecules/Avatar.jsx
+++ front-end/machinapp/src/components/molecules/Avatar.jsx
@@ -1,5 +1,5 @@
 import { DropDown } from "./navigation/Dropdown";
-import { DropdownItem, User } from "@nextui-org/react";
+import { User } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 import { Icons } from "../atoms/icons/Icons";
 
@@ -13,6 +13,30 @@ import {
 
 export const AvatarCom = () => {
   const { logout, user } = useAuth();
+
+  const ItemsDrop = [
+    <>
+      <p className="font-bold">{`${user.us_nombre} ${user.us_apellidos}`}</p>
+      <p className="font-bold">{user.us_correo}</p>
+    </>,
+    <>
+      <Link to={"/perfil"} className="flex gap-4">
+        <Icons icon={UserCircleIcon} /> <p>Administrar Perfil de usuario</p>
+      </Link>
+    </>,
+    <>
+      <Link className="flex gap-4">
+        <Icons icon={QuestionMarkCircleIcon} /> <p>Help & Feedbackp</p>
+      </Link>
+    </>,
+    <>
+      <div className="flex gap-4" onClick={logout}>
+        <Icons icon={ArrowRightOnRectangleIcon} />
+        <p> Salir</p>
+      </div>
+    </>,
+  ];
+
   return (
     <>
       <DropDown
@@ -21,40 +45,12 @@ export const AvatarCom = () => {
             className="cursor-pointer font-bold"
             avatarProps={{
               isBordered: true,
-              /*  src: "https://i.pravatar.cc/150?u=a042581f4e29026024d", */
             }}
             name={`${user.us_nombre} ${user.us_apellidos}`}
           />
         }
-      >
-        <DropdownItem key="profile" className="h-14 gap-2" textValue="re">
-          <p className="font-bold">{`${user.us_nombre} ${user.us_apellidos}`}</p>
-          <p className="font-bold">{user.us_correo}</p>
-        </DropdownItem>
-        <DropdownItem key="settings" textValue="fff">
-          <Link to={"/perfil"} className="flex gap-4">
-            <Icons icon={UserCircleIcon} /> <p>Administrar Perfil de usuario</p>
-          </Link>
-        </DropdownItem>
-
-        <DropdownItem key="help_and_feedback" textValue="tete">
-          <Link className="flex gap-4">
-            {" "}
-            <Icons icon={QuestionMarkCircleIcon} /> <p>Help & Feedbackp</p>
-          </Link>
-        </DropdownItem>
-        <DropdownItem
-          key="logout"
-          color="danger"
-          onPress={logout}
-          textValue="2e2"
-        >
-          <div className="flex gap-4">
-            <Icons icon={ArrowRightOnRectangleIcon} />
-            <p> Salir</p>
-          </div>
-        </DropdownItem>
-      </DropDown>
+        dropdown={ItemsDrop}
+      />
     </>
   );
 };
