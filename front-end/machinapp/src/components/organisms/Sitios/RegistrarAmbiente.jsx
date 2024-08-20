260ba6ada122325592662f6ac5bcc4a2e255edad
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2024-08-20 10:31:49 -0500

    registrar ambientes, areas y sedes

---
+++ front-end/machinapp/src/components/organisms/Sitios/RegistrarAmbiente.jsx
@@ -0,0 +1,10 @@
+import { FormAmbientes } from "../../components/organisms/formularios/FormAmbientes";
+import { Layout } from "../../index";
+
+export const RegistrarAmbiente = () => {
+  return (
+    <Layout>
+      <FormAmbientes />
+    </Layout>
+  )
+}
---
+++ front-end/machinapp/src/components/organisms/Sitios/RegistrarSede.jsx
@@ -0,0 +1,10 @@
+import { FormSedes } from '../../components/organisms/formularios/FormSedes';
+import { Layout } from "../../index";
+
+export const RegistrarSede = () => {
+  return (
+    <Layout>
+      <FormSedes />
+    </Layout>
+  )
+}
