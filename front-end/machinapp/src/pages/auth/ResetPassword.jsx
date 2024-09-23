a4a25a2879477e2111667e40a93451a0fef74e50
emersson <emerssonramirez510@gmail.com>
2024-09-23 16:06:45 -0500

    nuevo componente de layout para inicip de la app

--- front-end/machinapp/src/pages/auth/ResetPassword.jsx
+++ front-end/machinapp/src/pages/auth/ResetPassword.jsx
@@ -1,12 +1,4 @@
-import {
-  Header,
-  Footer,
-  InputforForm,
-  ModalComponte,
-  Login,
-  V,
-  axiosCliente,
-} from "../../index";
+import { InputforForm, V, axiosCliente, LayotuInicio } from "../../index";
 
 import { useForm } from "react-hook-form";
 import { Button } from "@nextui-org/react";
@@ -48,117 +40,55 @@ export const ResetPassword = () => {
 
   return (
     <>
-      <div className="hw-6/12">
-        <Header
-          color={"bg-white"}
-          contenido={
-            <ModalComponte
-              buttonModal={t("iniciar_sesion")}
-              tittleModal={t("iniciar_sesion")}
-              colorButton="primary"
-              componente={<Login />}
-            />
-          }
-        />
+      <LayotuInicio>
+        <div className="hw-6/12">
+          {/*  */}
+          <section className="bg-gray-100 flex flex-col h-screen ">
+            <div className="px-10 py-6">
+              <span className="text-4xl font-bold text-gray-800 mb-4"></span>
+              <div className="border-b-8 border-green-600 inline-block w-full"></div>
+            </div>
 
-        {/*  */}
-        <section className="bg-gray-100 flex flex-col h-screen ">
-          <div className="px-10 py-6">
-            <span className="text-4xl font-bold text-gray-800 mb-4"></span>
-            <div className="border-b-8 border-green-600 inline-block w-full"></div>
-          </div>
+            {/*   <!-- Imagen visible solo en pantallas grandes (1200px y mayores) --> */}
 
-          {/*   <!-- Imagen visible solo en pantallas grandes (1200px y mayores) --> */}
-
-          <div className="w-full  flex items-center justify-center">
-            <form
-              onSubmit={handleSubmit(handleSubmitData)}
-              className="w-full max-w-md flex flex-col gap-5 bg-white p-8 rounded-lg shadow-md"
-            >
-              <span className="font-semibold text-2xl lg:text-4xl text-custom-green">
-                {t("restablecer_contresena")}
-              </span>
-              <span className="text-base lg:text-lg">
-                {t("introduzca_numero_documento")}
-              </span>
-              <InputforForm
-                errors={errors}
-                tipo={"number"}
-                register={register}
-                name={"Documento"}
-                label={t("numero_documento")}
-              />
-              <Button
-                type="submit"
-                radius={V.Bradius}
-                isLoading={loading ? true : false}
-                className="text-white bg-blue-600 mt-4"
+            <div className="w-full  flex items-center justify-center">
+              <form
+                onSubmit={handleSubmit(handleSubmitData)}
+                className="w-full max-w-md flex flex-col gap-5 bg-white p-8 rounded-lg shadow-md"
               >
-                {t("restablecer")}
-              </Button>
+                <span className="font-semibold text-2xl lg:text-4xl text-custom-green">
+                  {t("restablecer_contresena")}
+                </span>
+                <span className="text-base lg:text-lg">
+                  {t("introduzca_numero_documento")}
+                </span>
+                <InputforForm
+                  errors={errors}
+                  tipo={"number"}
+                  register={register}
+                  name={"Documento"}
+                  label={t("numero_documento")}
+                />
+                <Button
+                  type="submit"
+                  radius={V.Bradius}
+                  isLoading={loading ? true : false}
+                  className="text-white bg-blue-600 mt-4"
+                >
+                  {t("restablecer")}
+                </Button>
 
-              {email && (
-                <div>
-                  la nueva contraseña fue enviada al correo:{" "}
-                  <span className="font-bold">{email}</span>{" "}
-                </div>
-              )}
-            </form>
-          </div>
-        </section>
-        <Footer />
-      </div>
+                {email && (
+                  <div>
+                    la nueva contraseña fue enviada al correo:
+                    <span className="font-bold">{email}</span>
+                  </div>
+                )}
+              </form>
+            </div>
+          </section>
+        </div>
+      </LayotuInicio>
     </>
   );
 };
-
-/* <div class="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-100">
-          
-            <div class="w-full md:w-1/2 flex items-center justify-center p-4">
-              
-            </div>
-
-            <div class="w-full md:w-1/2 bg-white p-6 rounded-lg shadow-lg">
-              <h2 class="text-2xl font-semibold text-gray-800 mb-6">
-                Formulario de Ejemplo
-              </h2>
-              <form>
-                <div class="mb-4">
-                  <label
-                    for="email"
-                    class="block text-gray-700 font-medium mb-2"
-                  >
-                    Correo Electrónico
-                  </label>  
-              us:ring-2 focus:ring-blue-600" required> 
-                </div>
-
-                <div class="mb-4">
-                  <label
-                    for="password"
-                    class="block text-gray-700 font-medium mb-2"
-                  >
-                    Contraseña
-                  </label>
-                </div>
-
-                <button
-                  type="submit"
-                  class="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-300"
-                >
-                  Iniciar Sesión
-                </button>
-              </form>
-            </div>
-          </div> */
-{
-  /*  <div className="hidden lg:w-1/2 lg:flex items-center justify-center p-4">
-            <div className="relative w-full h-[300px] lg:h-full overflow-hidden rounded-lg">
-              <LazyLoadImage
-                src="Saved.jpeg"
-                className="w-full h-full object-cover"
-                effect="blur"
-              />
-            </div>
-          </div> */
-}
