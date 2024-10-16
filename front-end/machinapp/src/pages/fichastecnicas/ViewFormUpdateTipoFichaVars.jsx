fb92ba3db88f8d57ccb93579043ae94552ca6987
Camilo3008 <camilo.alvarez3008@gmail.com>
2024-10-16 00:17:14 -0500

    añadiendo traduccion a las partes faltantes

--- front-end/machinapp/src/pages/fichastecnicas/ViewFormUpdateTipoFichaVars.jsx
+++ front-end/machinapp/src/pages/fichastecnicas/ViewFormUpdateTipoFichaVars.jsx
@@ -1,12 +1,15 @@
 import {Layout, Breadcrumb, FormUpdateVarsTipoFicha} from "../../index";
 
+import { useTranslation } from "react-i18next"
+
 
 export const ViewFormTipoFichaUpdate = ()=>{
 
+    const { t } = useTranslation();
     return(
         <>
         <Layout>
-            <Breadcrumb pageName={'Actualizar Variables Tipo de Ficha'} />
+            <Breadcrumb pageName={`${t('actVars')}`} />
             <FormUpdateVarsTipoFicha tipo_ficha = {'equipo'}/>
         </Layout>
         </>
