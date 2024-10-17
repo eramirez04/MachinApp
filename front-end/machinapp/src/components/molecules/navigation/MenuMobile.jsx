df9e7888f5028a98ddbd3191fe960963a66b096f
eramirez04 <emerssonramirez510@gmail.com>
2024-10-16 22:33:56 -0500

    eliminando componentes que no se utilizan

--- front-end/machinapp/src/components/molecules/navigation/MenuMobile.jsx
+++ front-end/machinapp/src/components/molecules/navigation/MenuMobile.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline/index.js";
-import { Link } from "react-router-dom";
-import { menus, Nav } from "../../../index";
+import { Nav } from "../../../index";
 
 const MenuMobile = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -34,24 +33,6 @@ const MenuMobile = () => {
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
             <Nav rol={"Administrador"} />
-
-            {/*    
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {menus.map((item) => (
-                  <Link
-                    key={item.name}
-                    to={item.link}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <div className="py-6"></div>
-            </div> */}
           </div>
         </Dialog.Panel>
       </Dialog>
